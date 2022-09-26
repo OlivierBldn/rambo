6b52d90dff904414ee525cca9e72e39b99a66c10
OlivierBldn <olivier.blandin@my-digital-school.org>
2022-09-26 10:56:02 +0200

    Creation of HTML, CSS, JS & ReadMe files

---
+++ ReadMe.md
@@ -0,0 +1,18 @@
+# ✋ Introduction
+
+This project is a course project. We, students of MyDigitalSchool, integrated a model with HTML and CSS using Git And GitHub.
+## ⚙ Starting
+
+To retrieve the project:
+
+```bash
+https://github.com/Camille38490/Evaluation-Git-B2-2022.git
+```
+
+Authors : 
+
+[Olivier Blandin](https://github.com/OlivierBldn)
+
+[Damien Baehr] (https://github.com/damienBaehr)
+
+Projet pédagogique pour MyDigitalSchool - Caen
---
+++ script.js
@@ -0,0 +1,21 @@
+function get($elem,$index='none'){
+
+    if($index==='none'){
+        if(document.querySelectorAll($elem).length > 1){
+        return document.querySelectorAll($elem)
+        }else{
+            return document.querySelector($elem)
+        }
+    }else{
+        return document.querySelectorAll($elem)[$index]
+    }	
+}
+
+function multipleEvents($elems,$event,$fn){
+
+    get($elems).forEach(elem=>{
+        elem.addEventListener($event, e=>{
+            return $fn(e)
+        })
+    })
+}
